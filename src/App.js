08f5afd6c400f335851b0ac0dd0fd883536c6bad
ImArnoldDCODES <danielsarnold010@gmail.com>
2022-06-30 15:17:24 -0700

    30th

--- src/App.js
+++ src/App.js
@@ -1,67 +1,86 @@
-import React, {useRef, useEffect} from 'react';
-import './App.scss';
-import arrow from './images/arrow-right.svg';
-import girl from './images/girl.webp';
-import boy from './images/ag.jpg';
-import { TweenMax, TimelineLite, Power3} from 'gsap'; 
+import React, { useRef, useEffect } from "react";
+import "./App.scss";
+import arrow from "./images/arrow-right.svg";
+import girl from "./images/girl.webp";
+import boy from "./images/ag.jpg";
+import { TweenMax, TimelineLite, Power3 } from "gsap";
 
 function App() {
-
   let App = useRef(null);
-  let images = useRef(null)
-  let tl = new TimelineLite({delay: .8});
+  let images = useRef(null);
+  let tl = new TimelineLite({ delay: 0.8 });
   let content = useRef(null);
 
   useEffect(() => {
-      // IMAGES
+    // IMAGES
 
-      const girlImage = images.firstElementChild;
-      const boyImage = images.lastElementChild;
+    const girlImage = images.firstElementChild;
+    const boyImage = images.lastElementChild;
 
+    // CONTENT HEADLINES
+    const headlineFirst = content.children[0].children[0];
+    const headlineSecond = headlineFirst.nextSibling;
+    const headlineThird = headlineSecond.nextSibling;
+    const contentP = content.children[1];
+    const contentButton = content.children[2];
 
-      // CONTENT HEADLLINES
-      const headlineFirst = content.children[0].children[0]
-      const headlineSecond = headlineFirst.nextSibling;
-      const headlineThird = headlineSecond.nextSibling;
-      const contentP = content.children[1]
-      const contentButton = content.children[2]
+    // IMAGE ANIMATION
+    TweenMax.to(App, 0, { css: { visibility: "visible" } });
+    tl.from(girlImage, 1.2, { y: 1280, ease: Power3.easeOut }, "Start")
+      .to(
+        girlImage.firstElementChild,
+        2,
+        { scale: 1.6, ease: Power3.easeOut },
+        0.2
+      )
+      .from(boyImage, { y: 1280, ease: Power3.easeOut }, 0.2)
+      .to(boyImage.lastElementChild, 2, {
+        scale: 1.6,
+        duration: 3,
+        ease: Power3.easeOut,
+      });
 
+    console.log(
+      headlineFirst,
+      headlineSecond,
+      headlineThird,
+      contentP,
+      contentButton
+    );
 
-      // IMAGE ANIMATION
-    TweenMax.to(App, 0, {css: {visibility: 'visible'}})
-    tl.from(girlImage, 1.2,{y: 1280, ease: Power3.easeOut}, 'Start')
-        .to(girlImage.firstElementChild, 2, {scale: 1.6, ease: Power3.easeOut}, .2)
-        .from(boyImage, {y: 1280, ease: Power3.easeOut}, .2)
-        .to(boyImage.lastElementChild, 2, {scale: 1.6, duration: 3, ease: Power3.easeOut})
+    // CONTENT ANIMATION
 
-        console.log(headlineFirst ,
-          headlineSecond,
-          headlineThird ,
-          contentP ,
-          contentButton )
+    tl.staggerFrom(
+      [headlineFirst.children, headlineSecond.children, headlineThird.children],
+      0.9,
+      {
+        y: 44,
+        ease: Power3.easeOut,
+        delay: 0.8,
+      },
+      0.2,
+      "Start"
+    )
+      .from(contentP, 1, { y: 20, opacity: 0, ease: Power3.easeOut }, 1.4)
+      .from(contentButton, 1, { y: 20, opacity: 0, ease: Power3.easeOut }, 1.6);
+  }, [tl]);
 
-          // CONTENT ANIMATION
-
-          tl.staggerFrom( [headlineFirst.children, headlineSecond.children, headlineThird.children,], .9, {
-            y: 44, ease: Power3.easeOut, delay: .8
-          }, .20, 'Start')
-          .from(contentP, 1, {y: 20, opacity: 0, ease: Power3.easeOut}, 1.4)
-          .from(contentButton, 1, {y: 20, opacity: 0, ease: Power3.easeOut}, 1.6)
-
-      },[tl]) 
- 
   return (
-    <div className="hero" ref={el => App = el}>
+    <div className="hero" ref={(el) => (App = el)}>
       <div className="container">
         <div className="hero-inner">
           <div className="hero-content">
-            <div className="hero-content-inner" ref={el => content = el}>
+            <div className="hero-content-inner" ref={(el) => (content = el)}>
               <h1>
                 <div className="hero-content-line">
-                  <div className="hero-content-line-inner">Relieving the burden</div>
+                  <div className="hero-content-line-inner">
+                    Relieving the burden
+                  </div>
                 </div>
                 <div className="hero-content-line">
-                  <div className="hero-content-line-inner">of disease caused</div>  
+                  <div className="hero-content-line-inner">
+                    of disease caused
+                  </div>
                 </div>
                 <div className="hero-content-line">
                   <div className="hero-content-line-inner">by behaviors.</div>
@@ -73,16 +92,17 @@ function App() {
                 digital therapeutics.
               </p>
               <div className="btn-row">
-                <button className="explore-button">Explore
+                <button className="explore-button">
+                  Explore
                   <div className="arrow-icon">
-                    <img src={arrow} alt="row"/>
+                    <img src={arrow} alt="row" />
                   </div>
                 </button>
               </div>
             </div>
           </div>
           <div className="hero-images">
-            <div className="hero-images-inner" ref={el => images = el}>
+            <div className="hero-images-inner" ref={(el) => (images = el)}>
               <div className="hero-image girl">
                 <img src={girl} alt="girl" />
               </div>
@@ -96,6 +116,5 @@ function App() {
     </div>
   );
 }
-
 
 export default App;
